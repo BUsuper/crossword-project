2313dbb470a6c6672797fa69a49492ff0332fdc9
BUsuper <merzlyakov96@gmail.com>
2025-05-14 19:24:54 +1000

    Wrap letter boxes into a table inside the field and make the borders collapse

--- src/components/CrosswordField/CrosswordField.jsx
+++ src/components/CrosswordField/CrosswordField.jsx
@@ -8,13 +8,26 @@ export default function CrosswordField() {
 
     const field = Array(rows).fill(0);
 
+    // TODO: wrap into a table and try to collapse borders
     return <div id="CrosswordField" style={{width: `${columns * COMPONENT_SIZE}px`, height: `${rows * COMPONENT_SIZE}px`}}>
-        {
-            field.map((row, rowNumber) => (
-                Array(columns).fill(0).map((column, columnNumber) => (
-                    <LetterBox x={columnNumber} y={rowNumber} key={`x${columnNumber}y${rowNumber}`}></LetterBox>
+        <table>
+            <tbody>
+            {
+                field.map((row, rowNumber) => (
+                    <tr key={`row${rowNumber}`}>
+                    {
+                        Array(columns).fill(0).map((column, columnNumber) => (
+                            <td key={`x${columnNumber}y${rowNumber}`}>
+                                {
+                                    <LetterBox x={columnNumber} y={rowNumber}></LetterBox>
+                                }
+                            </td>
+                        ))
+                    }
+                    </tr>
                 ))
-            ))
-        }
+            }
+            </tbody>
+        </table>
     </div>
 }
