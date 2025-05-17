9f847db772c52fc58fb5d45491a85fe75f4b1838
BUsuper <merzlyakov96@gmail.com>
2025-05-17 18:16:25 +1000

    Change crossword to an array and use map to render crossword cells

--- src/components/CrosswordField/CrosswordField.jsx
+++ src/components/CrosswordField/CrosswordField.jsx
@@ -14,10 +14,10 @@ export default function CrosswordField() {
         <table id="CrosswordField" style={{width: `${columns * COMPONENT_SIZE}px`, height: `${rows * COMPONENT_SIZE}px`}}>
             <tbody>
             {
-                field.map((row, rowNumber) => (
+                crossword.map((row, rowNumber) => (
                     <tr key={`row${rowNumber}`}>
                     {
-                        Array(columns).fill(0).map((column, columnNumber) => (
+                        row.map((column, columnNumber) => (
                             <td key={`x${columnNumber}y${rowNumber}`} className={!(crossword[rowNumber][columnNumber]) ? "emptyCell" : ""}>
                                 {
                                     <LetterBox x={columnNumber} y={rowNumber} letter={crossword[rowNumber][columnNumber]}></LetterBox>
