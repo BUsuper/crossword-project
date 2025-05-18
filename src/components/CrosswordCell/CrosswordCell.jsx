bf4c3f62676224478b01a319a044b4c09c5a0da9
BUsuper <merzlyakov96@gmail.com>
2025-05-18 17:17:04 +1000

    Make style improvements in crossword cells

--- src/components/CrosswordCell/CrosswordCell.jsx
+++ src/components/CrosswordCell/CrosswordCell.jsx
@@ -2,7 +2,19 @@ import "./CrosswordCell.css"
 
 export function CrosswordCell({x, y, letter, number}) {
     return <div className="CrosswordCell" id={`CrosswordCell(${y},${x})`}>
-        {number}
-        {letter}
+        <div className="upperRow">
+            <span className="numberContainter">
+                {number}
+            </span>
+            <span className="rightArrowContainer">
+                
+            </span>
+        </div>
+        <div className="bottomRow">
+            <span className="downArrowContainer"></span>
+            <span className="letterContainer">
+                {letter}
+            </span>
+        </div>
     </div>
 }
