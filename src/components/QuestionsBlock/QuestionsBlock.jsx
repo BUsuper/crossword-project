e89a6a94673e6547ae44e402eb9063030a79983b
BUsuper <merzlyakov96@gmail.com>
2025-05-18 18:43:31 +1000

    Add question block files

---
+++ src/components/QuestionsBlock/QuestionsBlock.jsx
@@ -0,0 +1,16 @@
+import crossword from "../../assets/crosswords"
+
+export default function QuestionsBlock({direction}) {
+    return <div>
+        <h1>{direction === "right" ? "Accross" : "Down"}</h1>
+        <div>
+            {crossword.map(row => {
+                row.map(cell => {
+                    if (cell[1] && cell[2] === direction) {
+                        return <p>{cell[0]}</p>
+                    }
+                })
+            })}
+        </div>
+    </div>
+}
---
+++ src/components/QuestionsSection/QuestionsSection.jsx
@@ -0,0 +1,8 @@
+import QuestionsBlock from "../../components"
+
+export default function QuestionsSection() {
+    return <div>
+        <QuestionsBlock direction="right"></QuestionsBlock>
+        <QuestionsBlock direction="down"></QuestionsBlock>
+    </div>
+}
