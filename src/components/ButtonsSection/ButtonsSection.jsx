f335e9f50bb1ad7edd808b200fa96fc3bd79a72b
BUsuper <merzlyakov96@gmail.com>
2025-05-30 17:07:24 +1000

    Change the button so that the checking mode can be turned off

--- src/components/ButtonsSection/ButtonsSection.jsx
+++ src/components/ButtonsSection/ButtonsSection.jsx
@@ -1,15 +1,23 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setIsChecking } from "../../slices/statusesSlice";
+import { selectIsChecking } from "../../slices/statusesSelectors";
 
 export function ButtonsSection() {
     const dispatch = useDispatch();
+    const isChecking = useSelector(selectIsChecking);
 
-    const checkCrossword = () => {
-        console.log("Checking the crossword");
-        dispatch(setIsChecking(true));
+    // Toggles checking mode
+    const toggleCheck = () => {
+        dispatch(setIsChecking(!isChecking));
     }
 
+    // It says "Change answers" because editing is disabled (in CrosswordCell) in checking mode
     return <div>
-        <input type="button" onClick={checkCrossword} value="Check"></input>
+        <input
+            type="button" 
+            onClick={toggleCheck} 
+            value={isChecking ? "Change answers" : "Check"}
+        >
+        </input>
     </div>
 }
