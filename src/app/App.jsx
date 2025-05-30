8e10dfb86c10b961393c41a5d2b99a19373fc3c8
BUsuper <merzlyakov96@gmail.com>
2025-05-30 13:41:11 +1000

    Adjust formatting

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,11 +2,12 @@ import { ButtonsSection, CrosswordField, QuestionsSection } from "../components"
 import "./App.css";
 
 function App() {
-  return <>
-    <CrosswordField></CrosswordField>
-    <ButtonsSection></ButtonsSection>
-    <QuestionsSection></QuestionsSection>
-  </>
-}
+  return (
+    <>
+      <CrosswordField></CrosswordField>
+      <ButtonsSection></ButtonsSection>
+      <QuestionsSection></QuestionsSection>
+    </>
+)}
 
 export default App
