6581b1229488c733a9a0fca2dc5c1d97038de81c
BUsuper <merzlyakov96@gmail.com>
2025-05-30 16:07:37 +1000

    Impement checking of user's answers

--- src/components/ButtonsSection/ButtonsSection.jsx
+++ src/components/ButtonsSection/ButtonsSection.jsx
@@ -1,6 +1,12 @@
+import { useDispatch } from "react-redux";
+import { setIsChecking } from "../../slices/statusesSlice";
+
 export function ButtonsSection() {
+    const dispatch = useDispatch();
+
     const checkCrossword = () => {
         console.log("Checking the crossword");
+        dispatch(setIsChecking(true));
     }
 
     return <div>
--- src/components/CrosswordCell/CrosswordCell.jsx
+++ src/components/CrosswordCell/CrosswordCell.jsx
@@ -1,20 +1,51 @@
-import "./CrosswordCell.css"
+import "./CrosswordCell.css";
+import { useSelector } from "react-redux";
+import { selectIsChecking } from "../../slices/statusesSelectors";
+import { useState } from "react";
 
+// Letter is the answer, userLetter is what user types in
 export function CrosswordCell({x, y, letter, number, direction}) {
-    return <div className="CrosswordCell" id={`CrosswordCell(${y},${x})`}>
-        <div className="upperRow">
-            <span className="numberContainter">
-                {number}
-            </span>
-            <span className="rightArrowContainer">
-                {direction === "right" ? "→" : ""}
-            </span>
+    // isChecking is triggered globally by a check button
+    const isChecking = useSelector(selectIsChecking);
+    const [userLetter, setUserLetter] = useState("");
+
+    const handleInputChange = e => {
+        setUserLetter(e.target.value);
+    }
+
+    // This is used when letter is rendered and checking mode is active to determine
+    // if the user's input is a correct answer and determine the input tag class
+    // accordingly to change the color of the letter
+    const isCorrectLetter = userLetter === letter;
+
+    return (
+        <div className="CrosswordCell" id={`CrosswordCell(${y},${x})`}>
+            <div className="upperRow">
+                <span className="numberContainter">
+                    {number}
+                </span>
+                <span className="rightArrowContainer">
+                    {direction === "right" ? "→" : ""}
+                </span>
+            </div>
+            <div className="bottomRow">
+                <span className="downArrowContainer">
+                    {direction === "down" ? "↓" : ""}
+                </span>
+                <input
+                    type="text" 
+                    id={`letter(${y},${x})`} 
+                    className={"letterContainer" + 
+                        `${isChecking ?
+                            isCorrectLetter ? " correctLetter" : " wrongLetter" :
+                            ""
+                        }`
+                        }
+                    maxLength={1}
+                    onChange={handleInputChange}
+                >
+                </input>
+            </div>
         </div>
-        <div className="bottomRow">
-            <span className="downArrowContainer">
-                {direction === "down" ? "↓" : ""}
-            </span>
-            <input type="text" id={`letter(${y},${x})`} className="letterContainer" maxLength={1}></input>
-        </div>
-    </div>
+    );
 }
