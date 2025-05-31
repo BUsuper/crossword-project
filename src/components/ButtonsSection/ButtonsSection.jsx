fe9a416c8d0a2aac0b67568a0d645993220f41ce
BUsuper <merzlyakov96@gmail.com>
2025-05-31 19:03:56 +1000

    Implement the finish crossword button

--- src/components/ButtonsSection/ButtonsSection.jsx
+++ src/components/ButtonsSection/ButtonsSection.jsx
@@ -1,14 +1,20 @@
 import { useDispatch, useSelector } from "react-redux";
-import { setIsChecking } from "../../slices/statusesSlice";
-import { selectIsChecking } from "../../slices/statusesSelectors";
+import { setIsChecking, setIsFinished } from "../../slices/statusesSlice";
+import { selectIsChecking, selectIsFinished } from "../../slices/statusesSelectors";
 
 export function ButtonsSection() {
     const dispatch = useDispatch();
     const isChecking = useSelector(selectIsChecking);
+    const isFinished = useSelector(selectIsFinished);
 
     // Toggles checking mode
     const toggleCheck = () => {
         dispatch(setIsChecking(!isChecking));
+    }
+
+    const finishCrossword = () => {
+        dispatch(setIsChecking(true));
+        dispatch(setIsFinished(true));
     }
 
     // It says "Change answers" because editing is disabled (in CrosswordCell) in checking mode
@@ -17,6 +23,14 @@ export function ButtonsSection() {
             type="button" 
             onClick={toggleCheck} 
             value={isChecking ? "Change answers" : "Check"}
+            disabled={isFinished}
+        >
+        </input>
+        <input
+            type="button"
+            onClick={finishCrossword}
+            value={"Finish"}
+            disabled={isFinished}
         >
         </input>
     </div>
