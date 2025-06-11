01a5bdf5683ad302b46c82d2c3710de04ff06f85
BUsuper <merzlyakov96@gmail.com>
2025-06-11 21:27:33 +1000

    Only render the modal when the puzzle is finished

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,13 +1,17 @@
+import { useSelector } from "react-redux";
 import { ButtonsSection, CrosswordField, EndgameModal, QuestionsSection } from "../components";
 import "./App.css";
+import { selectIsFinished } from "../slices/statusesSelectors";
 
 function App() {
+  const isFinished = useSelector(selectIsFinished);
+
   return (
     <>
       <CrosswordField></CrosswordField>
       <ButtonsSection></ButtonsSection>
       <QuestionsSection></QuestionsSection>
-      <EndgameModal></EndgameModal>
+      {isFinished && <EndgameModal></EndgameModal>}
     </>
 )}
 
