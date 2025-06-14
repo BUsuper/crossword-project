7c4f0a832a8d827dbec61c0b81bdc142e74c3650
BUsuper <merzlyakov96@gmail.com>
2025-06-14 20:34:42 +1000

    Add a button to close the EndgameModal

--- src/components/EndgameModal/EndgameModal.jsx
+++ src/components/EndgameModal/EndgameModal.jsx
@@ -1,14 +1,22 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import "./EndgameModal.css";
+import closeModalLogo from "../../assets/close.svg"
 import { selectHasErrors } from "../../slices/statusesSelectors";
+import { setIsEndgameModalOpen } from "../../slices/statusesSlice";
 
 export function EndgameModal() {
     const hasErrors = useSelector(selectHasErrors);
 
+    const dispatch = useDispatch();
+
     return (
         <div id="EndgameModal">
             <div className="modalUpperRow">
-                <img id="closeEndgameModalButton"></img>
+                <img 
+                    id="closeEndgameModalButton"
+                    src={closeModalLogo} 
+                    onClick={() => dispatch(setIsEndgameModalOpen(false))}>
+                </img>
             </div>
             <div className="modalResultsRow">
                 Congratulations! You've finished the crossword in 999 seconds
