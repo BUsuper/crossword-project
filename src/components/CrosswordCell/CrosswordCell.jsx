0011363b43a517c52397e46699c752a4a086dc3f
BUsuper <merzlyakov96@gmail.com>
2025-06-28 15:54:12 +1000

    Implement correct iteration using iterationOrder in cells

--- src/components/CrosswordCell/CrosswordCell.jsx
+++ src/components/CrosswordCell/CrosswordCell.jsx
@@ -6,7 +6,7 @@ import { setHasErrors } from "../../slices/statusesSlice";
 import { selectIsVerticalSelection, selectSelectedCell } from "../../slices/selectedSelectors";
 import { setIsVerticalSelection, setSelectedCell } from "../../slices/selectedSlice";
 
-export function CrosswordCell({x, y, correctAnswer, number, direction, isInSelectionList, tabIndex}) {
+export function CrosswordCell({x, y, correctAnswer, number, direction, isInSelectionList, iterationOrder, tabIndex}) {
     const isChecking = useSelector(selectIsChecking);
     const isShowingAnswers = useSelector(selectIsShowingAnswers);
     const selectedCell = useSelector(selectSelectedCell);
@@ -19,7 +19,41 @@ export function CrosswordCell({x, y, correctAnswer, number, direction, isInSelec
 
     const isCurrentlySelected = selectedCell === inputId;
 
-    const handleInputChange = (e, nextIndex) => {
+    const convertId = id => id.split(":").map(id => +id);
+
+    // Focuses on the next cell in the iteration order and changes selectedCell accordingly
+    const focusNext = (id, iterationOrder) => {
+        let currentId = convertId(id);
+        let currentIndex = iterationOrder.findIndex(
+            cellId => cellId[0] === currentId[0] && cellId[1] === currentId[1]
+        );
+
+        while (true) {
+            if (currentIndex < (iterationOrder.length - 1)) {
+                const nextCellId = iterationOrder[currentIndex + 1].join(":");
+                const nextCell = document.getElementById(nextCellId);
+                
+                if (!nextCell.value) {
+                    dispatch(setSelectedCell(nextCellId));
+                    nextCell.focus();
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+    }
+
+    const handleInputChange = (e, id, iterationOrder) => {
+        const userInput = e.target.value;
+        setUserLetter(userInput);
+
+        if (userInput.length > 0) {
+            focusNext(id, iterationOrder)
+        }
+    }
+
+    /* const handleInputChange = (e, nextIndex) => {
         const userInput = e.target.value;
         // Gets an iterable object with all Letter Containers
         const letterContainersList = document.getElementsByClassName("letterContainer");
@@ -36,6 +70,7 @@ export function CrosswordCell({x, y, correctAnswer, number, direction, isInSelec
 
         // TODO: add moving to the previous cell if it exists and backspace is pressed in an empty cell
     }
+        */
 
     // Make selection direction change if the same cell is clicked twice
     const handleClick = (currentCellId, isCurrentlySelected) => {
@@ -88,7 +123,7 @@ export function CrosswordCell({x, y, correctAnswer, number, direction, isInSelec
                         `${isInSelectionList ? "inSelectedList" : ""}`
                         }
                     maxLength={1}
-                    onChange={(e) => handleInputChange(e, tabIndex)}
+                    onChange={(e) => handleInputChange(e, inputId, iterationOrder)}
                     onClick={() => handleClick(inputId, isCurrentlySelected)}
                     disabled={isChecking}
                     autoComplete="off"
--- src/components/CrosswordField/CrosswordField.jsx
+++ src/components/CrosswordField/CrosswordField.jsx
@@ -86,6 +86,8 @@ export function CrosswordField() {
         return iterationOrder;
     }
 
+    const iterationOrder = createIterationOrder(crossword, columns, rows, "down");
+
     // Crossword cells are only rendered in non-empty table cells
     return (
         <table id="CrosswordField" style={{width: `${columns * COMPONENT_SIZE}px`, height: `${rows * COMPONENT_SIZE}px`}}>
@@ -108,6 +110,7 @@ export function CrosswordField() {
                                             number={crossword[rowNumber][columnNumber][2]}
                                             correctAnswer={crossword[rowNumber][columnNumber][3]}
                                             isInSelectionList={cellsInSelectionList.includes(`CrosswordCell${rowNumber},${columnNumber}`)}
+                                            iterationOrder={iterationOrder}
                                             tabIndex={++tabCounter}
                                         >
                                         </CrosswordCell>
