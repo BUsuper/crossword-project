447a691c056734ce72b71a86abdf5f07d16fcbfd
BUsuper <merzlyakov96@gmail.com>
2025-07-01 20:26:15 +1000

    Wrap the table in the CrosswordField into a div

--- src/components/CrosswordField/CrosswordField.jsx
+++ src/components/CrosswordField/CrosswordField.jsx
@@ -29,38 +29,40 @@ export function CrosswordField() {
     
     // Crossword cells are only rendered in non-empty table cells
     return (
-        <table id="CrosswordField" style={{width: `${columns * COMPONENT_SIZE}px`, height: `${rows * COMPONENT_SIZE}px`}}>
-            <tbody>
-            {
-                crossword.map((row, rowNumber) => (
-                    <tr key={`row${rowNumber}`}>
-                    {
-                        row.map((column, columnNumber) => (
-                            <td 
-                                key={`x${columnNumber}y${rowNumber}`} 
-                                className={!(crossword[rowNumber][columnNumber]) ? "emptyCell" : ""}
-                            >
-                                {
-                                    (crossword[rowNumber][columnNumber]) ?
-                                        <CrosswordCell
-                                            x={columnNumber}
-                                            y={rowNumber}
-                                            direction={crossword[rowNumber][columnNumber][1]}
-                                            number={crossword[rowNumber][columnNumber][2]}
-                                            correctAnswer={crossword[rowNumber][columnNumber][3]}
-                                            isInSelectionList={currentWord.includes(`${rowNumber},${columnNumber}`)}
-                                            tabIndex={++tabCounter}
-                                        >
-                                        </CrosswordCell>
-                                    : ""
-                                }
-                            </td>
-                        ))
-                    }
-                    </tr>
-                ))
-            }
-            </tbody>
-        </table>
+        <div id="CrosswordField">
+            <table style={{width: `${columns * COMPONENT_SIZE}px`, height: `${rows * COMPONENT_SIZE}px`}}>
+                <tbody>
+                {
+                    crossword.map((row, rowNumber) => (
+                        <tr key={`row${rowNumber}`}>
+                        {
+                            row.map((column, columnNumber) => (
+                                <td 
+                                    key={`x${columnNumber}y${rowNumber}`} 
+                                    className={!(crossword[rowNumber][columnNumber]) ? "emptyCell" : ""}
+                                >
+                                    {
+                                        (crossword[rowNumber][columnNumber]) ?
+                                            <CrosswordCell
+                                                x={columnNumber}
+                                                y={rowNumber}
+                                                direction={crossword[rowNumber][columnNumber][1]}
+                                                number={crossword[rowNumber][columnNumber][2]}
+                                                correctAnswer={crossword[rowNumber][columnNumber][3]}
+                                                isInSelectionList={currentWord.includes(`${rowNumber},${columnNumber}`)}
+                                                tabIndex={++tabCounter}
+                                            >
+                                            </CrosswordCell>
+                                        : ""
+                                    }
+                                </td>
+                            ))
+                        }
+                        </tr>
+                    ))
+                }
+                </tbody>
+            </table>
+        </div>
     );
 }
