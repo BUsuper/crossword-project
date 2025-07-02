51615e77db919021195ee845c932404ac3baecb8
BUsuper <merzlyakov96@gmail.com>
2025-07-02 18:43:36 +1000

    Save time to Redux state when the game is finished

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,17 +1,21 @@
 import { useEffect, useState } from "react";
 import "./Timer.css"
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import stopIndicator from "../../assets/stop.svg"
 import { selectIsFinished } from "../../slices/statusesSelectors";
+import { setTime } from "../../slices/statusesSlice";
 
 export function Timer() {
     const [elapsedTime, setElapsedTime] = useState(0);
     const isFinished = useSelector(selectIsFinished);
 
+    const dispatch = useDispatch();
+
     // The callback is called when the component mounts and when isFinished is changed
     useEffect(() => {
         // If isFinished don't start another timer
         if (isFinished) {
+            dispatch(setTime(elapsedTime));
             // If there is not timer, there is nothing to clean up
             return;
         }
