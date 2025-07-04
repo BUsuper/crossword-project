becb120012e7a9373365e190fe5fa716648e99d9
BUsuper <merzlyakov96@gmail.com>
2025-07-04 19:57:12 +1000

    Add a variable to save the name of the selected crossword in the Redux state

--- src/slices/selectedSelectors.js
+++ src/slices/selectedSelectors.js
@@ -1,2 +1,3 @@
 export const selectIsVerticalSelection = state => state.selected.isVerticalSelection;
 export const selectSelectedCell = state => state.selected.selectedCell;
+export const selectSelectedCrossword = state => state.selected.selectedCrossword;
