c5691b58ac2a01bbf70cb5c8bdbb95ab0b61ecdc
BUsuper <merzlyakov96@gmail.com>
2025-07-04 19:58:29 +1000

    Insert CrosswordSelection in the Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
+import { CrosswordSelection } from "../CrosswordSelection/CrosswordSelection";
 import { Timer } from "../Timer/Timer";
 
 export function Header() {
     return (
         <header>
             <Timer></Timer>
+            <CrosswordSelection></CrosswordSelection>
         </header>
     );
 }
