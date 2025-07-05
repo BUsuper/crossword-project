59ff0e0246d42c2f2278793493e115258ad7a968
BUsuper <merzlyakov96@gmail.com>
2025-07-05 15:41:26 +1000

    Add styles in the QuestionsSection and QuestionsBlocks

--- src/components/QuestionsBlock/QuestionsBlock.jsx
+++ src/components/QuestionsBlock/QuestionsBlock.jsx
@@ -1,3 +1,4 @@
+import "./QuestionsBlock.css"
 import { useSelector } from "react-redux"
 import { selectCrossword } from "../../slices/crosswordSelectors"
 
@@ -5,12 +6,12 @@ export function QuestionsBlock({direction}) {
     const crossword = useSelector(selectCrossword);
 
     return <div>
-        <h1>{direction === "right" ? "Across" : "Down"}</h1>
+        <h1 className="directionHeader">{direction === "right" ? "Across" : "Down"}</h1>
         <div>
             {crossword.map(row =>
                 row.map(cell => {
                     if (cell[0] && cell[1] === direction) {
-                        return <p key={`${cell[2]}${cell[1]}`}>{`${cell[2]}) ${cell[0]}`}</p>
+                        return <p key={`${cell[2]}${cell[1]}`} className="question">{`${cell[2]}) ${cell[0]}`}</p>
                     }
                 })
             )}
