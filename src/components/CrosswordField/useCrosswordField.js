e9c77ed67ced8b5edf2baf839b9c75706d092ff8
BUsuper <merzlyakov96@gmail.com>
2025-07-06 17:30:43 +1000

    Prevent selection direction change/change the direction automatically if the resulting selection has only one letter

--- src/components/CrosswordField/useCrosswordField.js
+++ src/components/CrosswordField/useCrosswordField.js
@@ -1,11 +1,14 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { selectSelectedCell, selectIsVerticalSelection } from "../../slices/selectedSelectors";
 import { selectCrossword } from "../../slices/crosswordSelectors";
 import { selectCurrentWord, filterDirection } from "../../utils/utils";
+import { setIsVerticalSelection } from "../../slices/selectedSlice";
 
 export default function useCrosswordField() {
     // Get height(rows) and width(columns) of the crossword object
     const crossword = useSelector(selectCrossword);
+
+    const dispatch = useDispatch();
 
     const [ selectedCellY, selectedCellX ] = useSelector(selectSelectedCell).split(":");
     const isVerticalSelection = useSelector(selectIsVerticalSelection);
@@ -17,6 +20,11 @@ export default function useCrosswordField() {
         .filter((cellId) => filterDirection(cellId, isVerticalSelection, selectedCellY, selectedCellX));
     
     const currentWord = selectCurrentWord(cellsInSelectionList, isVerticalSelection, selectedCellY, selectedCellX).map(id => id.join(","));
+
+    // This prevents selection when there is no actual word, just a single cell surrounded by emtpy cells
+    if (currentWord.length < 2) {
+        dispatch(setIsVerticalSelection(!isVerticalSelection));
+    }
 
     return {
         crossword,
